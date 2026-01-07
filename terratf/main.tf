22dac9569ec12b34e74d31b2115c7965e134c0bc
Anil Bamnote <anilbamnote21@gmail.com>
2026-01-07 14:36:25 +0530

    vpc-module-added

--- terratf/main.tf
+++ terratf/main.tf
@@ -3,5 +3,18 @@ provider "aws" {
 }
 
 module "my-vpc-module" {
-    source = ./module/vpc
+    source = "./module/vpc "
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket         = "s3-demo-backend-9886"
+    key            = "prod/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    use_lockfile = true
+    dynamodb_table = "new-db-table-s3"
+  }
 }
